267abe35b60720255f7c52ae838a5ee14d379212
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-14 10:46:06 +0200

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transform;
+
+namespace ConsoleApplication1 {
+	class Program {
+		static void Main(string[] args) {
+			Matrix x = new Matrix(new double[,]{
+				{1,2,3,4},
+				{5,6,7,8},
+				{9,10,11,12},
+				{13,14,15,16}
+			});
+			Console.WriteLine(x.ToString());
+			Console.WriteLine("");
+			Matrix y = new Matrix(new double[,]{
+				{1,2,3,4},
+				{5,6,7,8},
+				{9,10,11,12},
+				{13,14,15,16}
+			});
+			Console.WriteLine("add and subtrackt");
+			Matrix z = x + y;
+			Console.WriteLine(z);
+			z = x - y;
+			Console.WriteLine(z);
+			Console.WriteLine("Multiplication");
+			z = x * 2;
+			Console.WriteLine(z);
+			z =  2 * y;
+			Console.WriteLine(z);
+
+			z = x * y;
+			Console.WriteLine(z);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Transform/Matrix.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transform
+{
+	public delegate void receiveCoordinatedDataMember(int x, int y, double value);
+	public delegate void receiveDataMember(ref double value);
+	public class Matrix{
+
+		public double[,] data{
+			get; set; 
+		}
+
+		public Matrix() : this(new double[4, 4]){
+			ForEach( delegate(ref double value){
+				value = 0;
+			});
+			for(int y = 0; y < 4; y++){
+				for(int x = 0; x < 4; x++){
+					data[x,y] = 0;
+				}
+			}
+		}
+
+		public Matrix(double[,] data){
+			this.data = data;
+		}
+
+		public double sum(){
+			double result = 0;
+			ForEach(delegate(ref double value) {
+				result += value;
+			});
+			return result;
+		}
+		public override String ToString(){
+			String result = "";
+			ForEach(delegate(int x, int y, double value){
+			int prevY = 0;
+				if(x == 0){
+					result += "\n";
+				}
+				result += value + ",";
+
+			});
+			return result;
+		}
+
+		public void ForEachRow(int x,receiveDataMember closure){
+			for(int y = 0; y < data.GetLength(1); y++){
+				closure(ref data[y, x]);
+			}
+		}
+		public void ForEachColl(int y,receiveDataMember closure){
+			for(int x = 0; x < data.GetLength(0); x++){
+				closure(ref data[y, x]);
+			}
+		}
+		public void ForEach(receiveDataMember closure){
+			for(int y = 0; y < data.GetLength(1); y++){
+				for(int x = 0; x < data.GetLength(0); x++){
+					closure(ref data[y,x]);
+				}
+			}
+		}
+		public void ForEach(receiveCoordinatedDataMember closure){
+			for(int y = 0; y < data.GetLength(1); y++){
+				for(int x = 0; x < data.GetLength(0); x++){
+					closure(x, y, data[y,x]);
+				}
+			}
+		}
+		public static Matrix operator +(Matrix lhs, Matrix rhs){
+			Matrix result = new Matrix();
+			lhs.ForEach(
+			delegate(int x, int y, double value) {
+				result.data[y,x] = value + rhs.data[y,x];
+			});
+			return result;
+		}
+		public static Matrix operator -(Matrix lhs, Matrix rhs){
+			Matrix result = new Matrix();
+			lhs.ForEach(
+			delegate(int x, int y, double value) {
+				result.data[y,x] = value - rhs.data[y,x];
+			});
+			return result;
+		}
+		public static Matrix operator *(Matrix lhs, Matrix rhs){
+			Matrix result = new Matrix();
+			lhs.ForEach(
+			delegate(int x, int y, double value){
+				int i = 0;
+				rhs.ForEachColl(x, delegate(ref double one){
+					result.data[x,y] += one * lhs.data[i,y];
+					i++;
+				});
+			});
+			return result;
+		}
+
+		public static Matrix operator *(Matrix lhs, double rhs){
+			Matrix result = new Matrix();
+			lhs.ForEach(
+			delegate(int x, int y, double value) {
+				result.data[y,x] = value * rhs;
+			});
+			return result;
+		}
+
+		public static Matrix operator *(double lhs,
+			Matrix rhs){
+			return rhs * lhs;
+		}
+	}
+}
